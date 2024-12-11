24e46b24e9e5d6f57dc45e0e8c2812a6628cfb52
omoitsnan <nancylenguyen@yahoo.com>
2024-12-10 21:44:46 -0500

    three-frame animation support for player movement

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -16,6 +16,14 @@ public class Player extends Entity {
     GamePanel gp;
     KeyHandler keyIn;
 
+    BufferedImage up1, up2, up3;
+    BufferedImage down1, down2, down3;
+    BufferedImage right1, right2, right3;
+    BufferedImage left1, left2, left3;
+
+    int spriteCounter = 0;
+    int spriteNum = 1;
+
     public Player(GamePanel gp, KeyHandler keyIN){
         this.gp = gp;
         this.keyIn = keyIN;
@@ -38,48 +46,80 @@ public class Player extends Entity {
     public void getPlayerImage(){
 
         try{
-            up1 = ImageIO.read(getClass().getResourceAsStream("/Player/imageName.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("/Player/imageName.png"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("/Player/imageName.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/Player/imageName.png"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("/Player/imageName.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("/Player/imageName.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("/Player/imageName.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/Player/imageName.png"));
+            up1 = ImageIO.read(getClass().getResourceAsStream("/Player/player_up1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/Player/player_up2.png"));
+            up3 = ImageIO.read(getClass().getResourceAsStream("/Player/player_up3.png"));
 
-        }catch(IOException e){
+            down1 = ImageIO.read(getClass().getResourceAsStream("/Player/player_down1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/Player/player_down2.png"));
+            down3 = ImageIO.read(getClass().getResourceAsStream("/Player/player_down3.png"));
+
+            right1 = ImageIO.read(getClass().getResourceAsStream("/Player/player_right1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/Player/player_right2.png"));
+            right3 = ImageIO.read(getClass().getResourceAsStream("/Player/player_right3.png"));
+
+            left1 = ImageIO.read(getClass().getResourceAsStream("/Player/player_left1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/Player/player_left2.png"));
+            left3 = ImageIO.read(getClass().getResourceAsStream("/Player/player_left3.png"));
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
     public void update(){
+        boolean moving = false; 
+
         if (keyIn.upPressed){
             direction = "up";
             worldY -= speed;
+            moving = true;
         }
         else if (keyIn.downPressed){
             direction = "down";
             worldY += speed;
+            moving = true;
         }
         else if (keyIn.leftPressed){
             direction = "left";
             worldX -= speed;
+            moving = true;
         }
         else if (keyIn.rightPressed){
             direction = "right";
             worldX += speed;
+            moving = true;
         }
+
+        if(moving){
+            spriteCounter++;
+            if(spriteCounter > 10) {
+                spriteNum = (spriteNum % 3 ) + 1;
+                spriteCounter = 0;
+            }
+        } else {
+            spriteNum = 1;
+        }
+
     }
     public void draw(Graphics2D g2){
+        BufferedImage image = null;
 
-        BufferedImage image = switch (direction) {
-            case "up" -> up1;
-            case "down" -> down1;
-            case "right" -> right1;
-            case "left" -> left1;
-            default -> null;
-        };
+        switch (direction) {
+            case "up":
+                image = (spriteNum == 1) ? up1 : (spriteNum == 2) ? up2 : up3;
+                break;
+            case "down":
+                image = (spriteNum == 1) ? down1 : (spriteNum == 2) ? down2 : down3;
+                break;
+            case "left":
+                image = (spriteNum == 1) ? left1 : (spriteNum == 2) ? left2 : left3;
+                break;
+            case "right":
+                image = (spriteNum == 1) ? right1 : (spriteNum == 2) ? right2 : right3;
+                break;
+        }
 
-        g2.drawImage(image, screenX , screenY, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
     }
 
 }
